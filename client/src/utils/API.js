e5b48c88b78efc16012fae91ea71cdbd42c6fc97
Giselle556 <giselleborbolla@yahoo.com>
2021-11-07 03:15:23 -0700

    pj

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -56,8 +56,3 @@ export const searchGoogleBooks = (query) => {
   return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
 };
 
-// make a search to google books api
-// https://www.googleapis.com/books/v1/volumes?q=harry+potter
-export const searchGoogleBooks = (query) => {
-  return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
-};
