d002c15a082bffa8a7fa5c44580f6d33d77b74a4
Giselle556 <giselleborbolla@yahoo.com>
2021-11-07 02:56:03 -0700

    server

--- server/server.js
+++ server/server.js
@@ -13,23 +13,25 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-server.applyMiddleware({ app });
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+server.start()
+  .then(() => {
+    server.applyMiddleware({ app });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+    // Serve up static assets
+   if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, '../client/build')));
+   }
 
-db.once("open", () => {
+   app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+   });
+    db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`);
-    console.log(`use graphql at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
-});
+  });
+  })
